64025a8185314d78bfe857e67a07590d38dde5c7
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-19 15:28:18 -0800

    Done

--- GA_Nested_If_NicholasIngles/Program.cs
+++ GA_Nested_If_NicholasIngles/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.Write("Are you a student (yes/no): ");
+            string studentStatus = Console.ReadLine();
+
+            if (studentStatus.ToLower() == "yes")
+            {
+                Console.Write("Enter your GPA (0.0-4.0): ");
+                double gpa = double.Parse(Console.ReadLine());
+
+                if (gpa >= 3.0)
+                {
+                    Console.WriteLine("Congratulations! You are an adult with a good GPA.");
+                }
+                else
+                {
+                    Console.WriteLine("You are an adult student, but your GPA needs improvement.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You are an adult, but not a student.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("You are not an adult.");
+        }
+    }
+}
